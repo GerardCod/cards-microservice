6bbc64accb8e4a94c01ce90dc925d8d0e9e2024a
Gera <gerardo.aguilar.calderon@gmail.com>
2021-12-11 19:17:45 -0600

    modify: Cleaning imports in CardServiceImpl.java file

--- src/main/java/com/gerardo/microservice/cards/model/services/CardServiceImpl.java
+++ src/main/java/com/gerardo/microservice/cards/model/services/CardServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.gerardo.microservice.cards.model.dto.CardType;
 import com.gerardo.microservice.cards.model.dto.ServiceInput;
 import com.gerardo.microservice.cards.model.entities.Card;
 import com.gerardo.microservice.cards.model.repositories.CardsRepository;
