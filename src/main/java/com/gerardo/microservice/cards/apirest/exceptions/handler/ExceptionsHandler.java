b9efa724920c5abb8d1c9513ab7868dcc023777e
Gera <gerardo.aguilar.calderon@gmail.com>
2021-12-11 19:26:55 -0600

    modify: Adding comments to ExceptionsHandler class

--- src/main/java/com/gerardo/microservice/cards/apirest/exceptions/handler/ExceptionsHandler.java
+++ src/main/java/com/gerardo/microservice/cards/apirest/exceptions/handler/ExceptionsHandler.java
@@ -11,9 +11,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.gerardo.microservice.cards.apirest.exceptions.BadRequestException;
 import com.gerardo.microservice.cards.apirest.exceptions.NotFoundCardApplicableException;
 
+/**
+ * Controlador general de errores.
+ * @author Gerardo Aguilar
+ * @version 1.0.0
+ */
 @ControllerAdvice
 public class ExceptionsHandler {
 	
+	/**
+	 * Procesa la excepción cuando no se encuentran tarjetas aplicables al perfíl del usuario.
+	 * @param ex excepción de tipo NotFoundCardApplicableException.
+	 * @return regresa la respuesta del error.
+	 */
 	@ExceptionHandler(value = NotFoundCardApplicableException.class)
 	public ResponseEntity<Object> notFoundCardApplicable(NotFoundCardApplicableException ex) {
 		Map<String, Object> response = new HashMap<>();
@@ -21,6 +31,11 @@ public class ExceptionsHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
 	}
 	
+	/**
+	 * Procesa la excepción cuando el usuario ingresa información incorrecta.
+	 * @param ex excepción de tipo BadRequestException.
+	 * @return regresa la respuesta del error.
+	 */
 	@ExceptionHandler(value = BadRequestException.class)
 	public ResponseEntity<Object> badRequest(BadRequestException ex) {
 		Map<String, Object> response = new HashMap<>();
