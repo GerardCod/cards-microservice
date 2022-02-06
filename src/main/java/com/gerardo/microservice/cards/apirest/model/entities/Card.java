ed22e5acc6fae8a55cff8fbbaa924190e24e5a90
Gera <gerardo.aguilar.calderon@gmail.com>
2022-02-06 16:24:22 -0600

    modify: Agregando métodos equals, hashCode y toString en la clase Card

--- src/main/java/com/gerardo/microservice/cards/apirest/model/entities/Card.java
+++ src/main/java/com/gerardo/microservice/cards/apirest/model/entities/Card.java
@@ -1,6 +1,7 @@
 package com.gerardo.microservice.cards.apirest.model.entities;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,13 +13,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Entity
 public class Card {
 	@Id
@@ -42,4 +41,31 @@ public class Card {
 	
 	@Column(nullable = false)
 	private String cardName;
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("Card{");
+		sb.append("id=").append(id);
+		sb.append(", passion='").append(passion).append('\'');
+		sb.append(", minMonthlySalary=").append(minMonthlySalary);
+		sb.append(", maxMonthlySalary=").append(maxMonthlySalary);
+		sb.append(", minAge=").append(minAge);
+		sb.append(", maxAge=").append(maxAge);
+		sb.append(", cardName='").append(cardName).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Card card = (Card) o;
+		return id.equals(card.id) && passion.equals(card.passion) && cardName.equals(card.cardName);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, passion, cardName);
+	}
 }
