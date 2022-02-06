f4db856c06984c2b1fa337250eb6e41fd7f2b19b
Gera <gerardo.aguilar.calderon@gmail.com>
2022-02-06 16:36:29 -0600

    modify: Actualizando la documentación del controlador de errores

--- src/main/java/com/gerardo/microservice/cards/apirest/exceptions/handler/ExceptionsHandler.java
+++ src/main/java/com/gerardo/microservice/cards/apirest/exceptions/handler/ExceptionsHandler.java
@@ -9,44 +9,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.gerardo.microservice.cards.apirest.exceptions.NotFoundCardApplicableException;
 
 /**
- * Controlador general de errores.
+ * Controlador para procesar los errores de los controladores.
  * @author Gerardo Aguilar
  * @version 1.0.0
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionsHandler {
 	
 	/**
-	 * Procesa la excepción cuando no se encuentran tarjetas aplicables al perfíl del usuario.
+	 * Captura el error cuando no se encuentran tarjetas aplicables a un cliente.
 	 * @param ex excepción de tipo NotFoundCardApplicableException.
 	 * @return regresa la respuesta del error.
 	 */
 	@ExceptionHandler(value = NotFoundCardApplicableException.class)
-	public ResponseEntity<Object> notFoundCardApplicable(NotFoundCardApplicableException ex) {
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public Map<String, Object> notFoundCardApplicable(NotFoundCardApplicableException ex) {
 		Map<String, Object> response = new HashMap<>();
 		response.put("message", ex.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+		return response;
 	}
 	
 	/**
-	 * Captura el error de cuando hace falta un parámetro en la petición de tarjetas.
+	 * Captura el error cuando hace falta un parámetro en la petición de tarjetas.
 	 * @param ex objeto de tipo MissingSerlvletRequestParameterException
 	 * @return regresa un objeto de tipo ResponseEntity
 	 */
 	@ExceptionHandler(value = MissingServletRequestParameterException.class)
-	public ResponseEntity<Object> missingRequestParameters(MissingServletRequestParameterException ex) {
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Map<String, Object> missingRequestParameters(MissingServletRequestParameterException ex) {
 		Map<String, Object> response = new HashMap<>();
 		
 		response.put("message", "Olvidaste agregar alguno de los siguientes parámatros a la petición");
 		response.put("param", ex.getParameterName());
 		response.put("paramType", ex.getParameterType());
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		return response;
 	}
 	
 	/**
@@ -56,14 +59,14 @@ public class ExceptionsHandler {
 	 * @return regresa un objeto de tipo ResponseEntity.
 	 */
 	@ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
-	public ResponseEntity<Object> paramTypeMismatch(MethodArgumentTypeMismatchException ex) {
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public Map<String, Object> paramTypeMismatch(MethodArgumentTypeMismatchException ex) {
 		Map<String, Object> response = new HashMap<>();
 		
 		response.put("message", "El tipo de dato que enviaste no coincide con el esperado");
 		response.put("param", ex.getName());
 		response.put("paramTypeExpected", ex.getParameter().getParameterType());
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+		return response;
 	}
 	
 }
